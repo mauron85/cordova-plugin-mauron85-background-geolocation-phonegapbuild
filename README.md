60f49171752422583a11715982974c655d929289
Marian Hello <marian.hello@gmail.com>
2015-10-13 10:30:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+cordova-plugin-mauron85-background-geolocation-phonegapbuild
+==============================
+
+Fork notice
+==============================
+
+This is fork of my [cordova-plugin-background-geolocation](https://github.com/mauron85/cordova-plugin-background-geolocation).
+The only reason to fork is that [Adobe® PhoneGap™ Build](http://build.phonegap.com) does not support external frameworks like google-play-services directly.
+
+The documentation and code base will stay at former location [cordova-plugin-background-geolocation](https://github.com/mauron85/cordova-plugin-background-geolocation).
+
+## Registering plugin for Adobe® PhoneGap™ Build
+
+[Adobe® PhoneGap™ Build](http://build.phonegap.com) supports plugins from npm as well.
+
+To register plugin add following line into your config.xml:
+
+```
+<gap:plugin name="cordova-plugin-mauron85-background-geolocation-phonegapbuild" source="npm"/>
+```
+
+NOTE: If you're using *hydration*, you have to download and reinstall your app with every new version of the plugin, as plugins are not updated.
+
+## Licence ##
+
+[Apache License](http://www.apache.org/licenses/LICENSE-2.0)
+
+Copyright (c) 2013 Christopher Scott, Transistor Software
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ src/android/BackgroundGpsPlugin.java
@@ -0,0 +1,158 @@
+/*
+According to apache license
+
+This is fork of christocracy cordova-plugin-background-geolocation plugin
+https://github.com/christocracy/cordova-plugin-background-geolocation
+
+Differences to original version:
+
+1. new methods isLocationEnabled, mMessageReciever, handleMessage
+*/
+
+package com.tenforwardconsulting.cordova.bgloc;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.text.TextUtils;
+import android.util.Log;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.marianhello.cordova.bgloc.Config;
+import com.marianhello.cordova.bgloc.Constant;
+import com.marianhello.cordova.bgloc.ServiceProvider;
+
+public class BackgroundGpsPlugin extends CordovaPlugin {
+    private static final String TAG = "BackgroundGpsPlugin";
+
+    public static final String ACTION_START = "start";
+    public static final String ACTION_STOP = "stop";
+    public static final String ACTION_CONFIGURE = "configure";
+    public static final String ACTION_SET_CONFIG = "setConfig";
+    public static final String ACTION_LOCATION_ENABLED_CHECK = "isLocationEnabled";
+
+    private Config config = new Config();
+    private Boolean isEnabled = false;
+    private Intent updateServiceIntent;
+    private CallbackContext callbackContext;
+
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) {
+        Activity activity = this.cordova.getActivity();
+        Context context = activity.getApplicationContext();
+
+        if (ACTION_START.equalsIgnoreCase(action) && !isEnabled) {
+            try {
+                updateServiceIntent = new Intent(activity, ServiceProvider.getClass(config.getLocationServiceProvider()));
+            } catch (ClassNotFoundException e) {
+                callbackContext.error("Configuration error: provider not found");
+                return false;
+            }
+
+            IntentFilter intentFilter = new IntentFilter(Constant.FILTER);
+            context.registerReceiver(mMessageReceiver, intentFilter);
+
+            updateServiceIntent.putExtra("config", config);
+            updateServiceIntent.putExtra("activity", cordova.getActivity().getClass().getCanonicalName());
+            Log.d( TAG, "Put activity " + cordova.getActivity().getClass().getCanonicalName() );
+
+            activity.startService(updateServiceIntent);
+            isEnabled = true;
+            Log.d(TAG, "bg service has been started");
+
+        } else if (ACTION_STOP.equalsIgnoreCase(action)) {
+            context.unregisterReceiver(mMessageReceiver);
+            isEnabled = false;
+            activity.stopService(updateServiceIntent);
+            callbackContext.success();
+            Log.d(TAG, "bg service has been stopped");
+        } else if (ACTION_CONFIGURE.equalsIgnoreCase(action)) {
+            try {
+                this.callbackContext = callbackContext;
+                this.config = Config.fromJSONArray(data);
+                Log.d(TAG, "bg service configured");
+            } catch (JSONException e) {
+                callbackContext.error("Configuration error: " + e.getMessage());
+                return false;
+            }
+        } else if (ACTION_SET_CONFIG.equalsIgnoreCase(action)) {
+            // TODO reconfigure Service
+            callbackContext.success();
+            Log.d(TAG, "bg service reconfigured");
+        } else if (ACTION_LOCATION_ENABLED_CHECK.equalsIgnoreCase(action)) {
+            Log.d(TAG, "location services enabled check");
+            try {
+                int isLocationEnabled = BackgroundGpsPlugin.isLocationEnabled(context) ? 1 : 0;
+                callbackContext.success(isLocationEnabled);
+            } catch (SettingNotFoundException e) {
+                callbackContext.error("Location setting not found on this platform");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Override method in CordovaPlugin.
+     * Checks to see if it should turn off
+     */
+    public void onDestroy() {
+        Activity activity = this.cordova.getActivity();
+
+        if (isEnabled && config.getStopOnTerminate()) {
+            activity.stopService(updateServiceIntent);
+        }
+    }
+
+    public static boolean isLocationEnabled(Context context) throws SettingNotFoundException {
+        int locationMode = 0;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+        } else {
+            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+    }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "Received location from bg service");
+            handleMessage(intent);
+        }
+    };
+
+    private void handleMessage(Intent msg) {
+        Bundle data = msg.getExtras();
+        switch (data.getInt(Constant.COMMAND, 0))
+        {
+            case Constant.UPDATE_PROGRESS:
+                try {
+                    JSONObject location = new JSONObject(data.getString(Constant.DATA));
+                    PluginResult result = new PluginResult(PluginResult.Status.OK, location);
+                    result.setKeepCallback(true);
+                    callbackContext.sendPluginResult(result);
+                    Log.d(TAG, "Sending plugin result");
+                } catch (JSONException e) {
+                    Log.w(TAG, "Error converting message to json");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
